76a7331b3be47f01ae1652a071355f0af9742604
Calvin McCarter <cmccarter@bighatbio.com>
2025-04-03 22:25:12 -0400

    abnumber

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "iglm",
         "pyarrow",
         "scikit-learn"
+        r"abnumber @ git+https://github.com/prihoda/AbNumber.git@e5d8e2bf89b8ae28b9f435f3061f3ad3bdd102fb#egg=abnumber",
     ],
     python_requires="==3.12",
 )
